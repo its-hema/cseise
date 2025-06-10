dc34c707d335ff8b656e397e0caad1a2945bf11f
its-hema <hema.22cse@cambridge.edu.in>
2025-06-10 05:49:48 +0530

    inital commit with jenkins

---
+++ procse/src/main/java/procse6/App.java
@@ -0,0 +1,13 @@
+package procse6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
